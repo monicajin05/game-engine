d4ac9ecf93aee7f90383c053a20bf6925dd02cf9
Monica Jin <monica.j.nc19@gmail.com>
2026-09-03 15:58:00 -0400

    add addl. attributes to entity, parent child system

---
+++ src/entity/Entity.cpp
@@ -0,0 +1,46 @@
+#include "Entity.h"
+
+/**
+ * @author mjin6
+ */
+
+Entity::Entity(float x, float y, float width, float height)
+    : x_(x), y_(y), width_(width), height_(height) {}
+
+void Entity::update() {
+    movement();
+    if (hasPhysics_) {
+        x_ += vx_;
+        y_ += vy_;
+    }
+    if (sprite_) {
+        sprite_->update();
+    }
+    for (auto& child : children_) {
+        child->update();
+    }
+}
+
+void Entity::draw(SDL_Renderer* renderer) const {
+    if (!visible_) {
+        return;
+    }
+    if (sprite_) {
+        sprite_->draw(renderer, worldX(), worldY());
+    }
+    for (auto& child : children_) {
+        child->draw(renderer);
+    }
+}
+
+// Position is the center of the entity
+SDL_FRect Entity::hitbox() const {
+    return SDL_FRect{worldX() - width_ / 2.0f, worldY() - height_ / 2.0f, width_, height_};
+}
+
+// function to add child entity to this entity
+Entity& Entity::addChild(std::unique_ptr<Entity> child) {
+    child->parent_ = this;
+    children_.push_back(std::move(child));
+    return *children_.back();
+}
